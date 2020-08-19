f0f5ff9c0d00138d14704af0cfcf10fba6190f65
wlcreate <waverley.nyc@gmail.com>
2020-08-18 20:11:57 -0400

    adds reading the user's reservations, and user choosing a reservation

--- app/models/interface.rb
+++ app/models/interface.rb
@@ -8,13 +8,9 @@ class Interface
 
     def welcome
         # self -> instance of the Interface
-
         # choice = prompt.select("Welcome to our Application. What would you like to do?", ["See All Plants", "See All Categories", "Create A User"])
-
         # if choice == "See All Plants" 
-
         # elsif choice == "See All Categories"
-
         # end
 
         prompt.select("Please make a selection:") do |menu|
@@ -58,36 +54,78 @@ class Interface
     end
 
     #displays all of the user's reservations
-    #Need help with making the user's reservations a choice so that they can either update or delete
+    #user chooses one of their reservations
+    #Need help with making the user's reservations a choice so that they can either update or delete - Wave did with Sylwia!
     def display_user_reservations_helper
         # self.user <- User who is logged in
         # self.user.restaurants <- All of the restaurants associated with the User
-        # self.user.reservations <- All of the reservation instances 
-        puts "Here are all of your reservations"
+        # self.user.reservations <- All of the reservation instances
+        # refer to Eric's video around 1:22:55 for more info on getting specific info out of objects! 
+        all_choices = []
         self.user.reservations.each do |user_reservation|
-            puts "#{user_reservation.date} - #{user_reservation.restaurant.name}"
-            puts "*******"
+            all_choices << "#{user_reservation.date} - #{user_reservation.restaurant.name}"
+        end
+        choice = prompt.select("What reservation do you want to see?", all_choices)
+        see_chosen_reservation(choice)
+
+        # sleep 5 #after 5 seconds of inactivity
+        # self.main_menu #goes back to main menu
+        # # self.main_menu <- To take me back to the main_menu
+    end
+
+    #displays the chosen reservation
+    #asks if the user want to update or cancel the chosen reservation
+    #do we want to display the chosen restaurant's more info again?
+    def see_chosen_reservation(chosen_reservation_instance)
+        puts "You have a reservation at #{chosen_reservation_instance.restaurant.name} on #{chosen_reservation_instance.date}"
+        
+        #Sylwia's way
+        all_choices = ["update", "cancel", "back"]
+        choice = prompt.select("What do you want do?", all_choices)
+        if choice == "update"
+          update_reservation(chosen_reservation_instance)
+        elsif choice == "cancel"
+          delete_reservation(chosen_reservation_instance)
+        elsif choice == "back"
+            self.main_menu
         end
 
-        sleep 5 #after 5 seconds of inactivity
-        self.main_menu #goes back to main menu
-        # self.main_menu <- To take me back to the main_menu
+        # #Eric's way - I'm pretty sure this is the same
+        # prompt.select("What would you like to do?") do |menu|
+        #     menu.choice "update", -> {update_reservation(chosen_reservation_instance)}
+        #     menu.choice "cancel", -> { delete_reservation(chosen_reservation_instance) }
+        #     menu.choice "back", -> {self.main_menu}
+        # end
+    end
+
+     #updates the chosen reservation
+     #can choose to update the date and/or the party_size
+     def update_reservation(chosen_reservation_instance)
+        #code for updating
+        puts "updated!"
+    end
+
+    #deletes the chosen reservation
+    def delete_reservation(chosen_reservation_instance)
+        #code for deleting
+        puts "cancelled"
     end
 
     #displays all of the restaurants
     #creates a new reservation
+    #Note: I think that this can be refractored, perhaps can look at above to see where/how - Wave
     def display_and_add_reservations_helper
         # Restaurant.all_names is defined in the Restaurant class and shows all restaurants => [{name => id}, {name => id}]
 
         chosen_restaurant_id = prompt.select("View all of our participating restaurants", Restaurant.all_names)
 
-        #need to display the more information about the chosen restaurant
+        #Need to display the "more information" about the chosen restaurant
 
         #after seeing more information about a restaurant, ask if user wants to make a reservation
         #if yes, create a new reservation
         #if no, bring back to see all restaurants or go to main menu
         if prompt.yes?("Do you want to make a reservation?")
-            reservation_date = TTY::Prompt.new.ask("What date and time would you like to reserve?")
+            reservation_date = TTY::Prompt.new.ask("What date and time would you like to dine?") #haha I rhymed
             reservation_party_size = TTY::Prompt.new.ask("How many people?")
             new_reservation = Reservation.create(guest_id: self.user.id, restaurant_id: chosen_restaurant_id, date: reservation_date, party_size: reservation_party_size)
         else
